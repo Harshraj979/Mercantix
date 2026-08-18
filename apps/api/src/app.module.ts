db8410d15bb4780d01fcc1b4e6dacd4f33b2f205
{harsh <hr5300439@gmail.com>
2026-08-18 13:31:27 +0530

    feat: added swagger and root module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,2 +1,45 @@
-// NestJS Root Module - write your app imports here
-export {};
+// NestJS Root Module
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
+import { AppController } from "./app.controller";
+import { HttpExceptionFilter } from "@common/filters/http-exception.filter";
+import { LoggingInterceptor } from "@common/interceptors/logging.interceptor";
+import { TransformInterceptor } from "@common/interceptors/transform.interceptor";
+import { PrismaModule } from "@common/prisma/prisma.module";
+import configuration from "./config/configuration";
+import { validate } from './config/env.validation';
+
+@Module({
+    imports: [
+        //1. Load and validate env globally
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [configuration],
+            validate,
+        }),
+        //2. GLobal database connection 
+        PrismaModule,
+    ],
+    controllers: [AppController],
+    providers: [
+        {
+            //1. Provide global exception handler
+            provide: APP_FILTER,
+            useClass: HttpExceptionFilter,
+
+        },
+        {
+            //2. Register global request logging interceptor
+
+            provide: APP_INTERCEPTOR,
+            useClass: LoggingInterceptor,
+        },
+        {
+            // 3. register global transform interceptors for the app
+            provide: APP_INTERCEPTOR,
+            useClass: TransformInterceptor,
+        },
+    ]
+})
+export  class AppModule{ };
