d52bf6e804131be82a6dbfc70b49340513c27d53
{harsh <hr5300439@gmail.com>
2026-08-23 16:32:20 +0530

    feat(auth): add CurrentUser parameter decorator

--- apps/api/src/common/decorators/current-user.decorator.ts
+++ apps/api/src/common/decorators/current-user.decorator.ts
@@ -1,1 +1,14 @@
-export {};
+import { createParamDecorator,ExecutionContext } from "@nestjs/common";
+import {JwtPayLoad} from '@mercantix/contracts'
+
+export const CurrentUser=createParamDecorator(
+    (data:keyof JwtPayLoad|undefined,ctx:ExecutionContext)=>{
+        const request=ctx.switchToHttp().getRequest();
+        const user=request.user as JwtPayLoad;
+
+        if(!user){
+            return null;
+        }
+        return data?user[data]:user;
+    }
+)
